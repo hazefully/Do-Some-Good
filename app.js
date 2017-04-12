7e6040dff1364ff119b3aa68b38eaad05e8744d1
Abdelrahman Ramadan <abdelrahman0xff@gmail.com>
2017-04-12 23:18:37 +0200

    mod3

--- app.js
+++ app.js
@@ -22,8 +22,103 @@ app.get("/webhook", function (req, res) {
     res.sendStatus(403);
   }
 });
+app.post('/webhook', function (req, res) {
+	var data = req.body;
+
+	if (data.object === 'page') {
+
+		// Iterate over each entry - there may be multiple if batched
+		data.entry.forEach(function (entry) {
+				var pageID = entry.id;
+				var timeOfEvent = entry.time;
+
+				//Iterate over each messaging event
+				entry.messaging.forEach(function(event) {
+					if(event.message) {
+						receivedMessage(event);
+					} else {
+						console.log("Webhook recieved unknown event: ", event);
+					}
+				});
+		});
+		res.sendStatus(200);
+	}
+});
+
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  console.log("Received message for user %d and page %d at %d with message:", 
+    senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var messageId = message.mid;
+
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+
+  if (messageText) {
+
+    // If we receive a text message, check to see if it matches a keyword
+    // and send back the example. Otherwise, just echo the text we received.
+    switch (messageText) {
+      case 'generic':
+        sendGenericMessage(senderID);
+        break;
+
+      default:
+        sendTextMessage(senderID, messageText);
+    }
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, "Message with attachment received");
+  }
+}
+
+function sendGenericMessage(recipientId, messageText) {
+  // To be expanded in later sections
+}
+
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  };
+
+  callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s", 
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });  
+}
+
+
 
 app.listen((process.env.PORT || 5000), function () {
-	console.log('Server Started on Port ' + (process.env.PORT || 5000));
+	console.log("Server Started on Port %d" (process.env.PORT || 5000));
 });
 
