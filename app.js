de9d85afb616313c26b1487401b8adcd0509e503
Hazem Essam <hazem.essam@gmail.com>
2017-04-25 18:26:47 +0200

    why

--- app.js
+++ app.js
@@ -37,8 +37,8 @@ app.post('/webhook', function (req, res) {
 					if(event.message) {
 						receivedMessage(event);
 					} else if(event.postback) {
-            console.log("I am supposed to handle something here?!");
-            // processPostback(event);
+            // console.log("I am supposed to handle something here?!");
+            processPostback(event);
           }
           else {
 						console.log("Webhook recieved unknown event: ", event);
@@ -86,29 +86,30 @@ function receivedMessage(event) {
 function processPostback(event) {
   var senderId = event.sender.id;
   var payload = event.postback.payload;
-
+  console.log(payload);
   if (payload === "Greeting") {
     // // Get user's first name from the User Profile API
     // // and include it in the greeting
-    request({
-      url: "https://graph.facebook.com/v2.6/" + senderId,
-      qs: {
-        access_token: process.env.PAGE_ACCESS_TOKEN,
-        fields: "first_name"
-      },
-      method: "GET"
-    }, function(error, response, body) {
-      var greeting = "";
-      if (error) {
-        console.log("Error getting user's name: " +  error);
-      } else {
-        var bodyObj = JSON.parse(body);
-        name = bodyObj.first_name;
-        greeting = "Hi " + name + ". ";
-      }
-      var message = greeting + "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
-      sendMessage(senderId, {text: message});
-    });
+    // request({
+    //   url: "https://graph.facebook.com/v2.6/" + senderId,
+    //   qs: {
+    //     access_token: process.env.PAGE_ACCESS_TOKEN,
+    //     fields: "first_name"
+    //   },
+    //   method: "GET"
+    // }, function(error, response, body) {
+    //   var greeting = "";
+    //   if (error) {
+    //     console.log("Error getting user's name: " +  error);
+    //   } else {
+    //     var bodyObj = JSON.parse(body);
+    //     name = bodyObj.first_name;
+    //     greeting = "Hi " + name + ". ";
+    //   }
+    //   var message = greeting + "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
+    //   sendTextMessage(senderId, {text: message});
+    // });
+    sendGenericMessage(senderID);
   }
 }
 
