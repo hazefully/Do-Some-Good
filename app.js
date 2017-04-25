800663af3fe86bc25b73330eaaf3d6e23a68ef12
Hazem Essam <hazem.essam@gmail.com>
2017-04-25 18:05:48 +0200

    dsds

--- app.js
+++ app.js
@@ -36,7 +36,10 @@ app.post('/webhook', function (req, res) {
 				entry.messaging.forEach(function(event) {
 					if(event.message) {
 						receivedMessage(event);
-					} else {
+					} else if(event.postback) {
+            processPostback(event);
+          }
+          else {
 						console.log("Webhook recieved unknown event: ", event);
 					}
 				});
@@ -74,6 +77,37 @@ function receivedMessage(event) {
     }
   } else if (messageAttachments) {
     sendTextMessage(senderID, "Message with attachment received");
+  }
+}
+
+
+
+function processPostback(event) {
+  var senderId = event.sender.id;
+  var payload = event.postback.payload;
+
+  if (payload === "Greeting") {
+    // Get user's first name from the User Profile API
+    // and include it in the greeting
+    request({
+      url: "https://graph.facebook.com/v2.6/" + senderId,
+      qs: {
+        access_token: process.env.PAGE_ACCESS_TOKEN,
+        fields: "first_name"
+      },
+      method: "GET"
+    }, function(error, response, body) {
+      var greeting = "";
+      if (error) {
+        console.log("Error getting user's name: " +  error);
+      } else {
+        var bodyObj = JSON.parse(body);
+        name = bodyObj.first_name;
+        greeting = "Hi " + name + ". ";
+      }
+      var message = greeting + "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
+      sendMessage(senderId, {text: message});
+    });
   }
 }
 
