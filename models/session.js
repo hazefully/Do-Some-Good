29f868c880b0ca340ade47cd1a6ed1b68d3b8f74
Abdelrahman Ramadan <abdelrahman0xff@gmail.com>
2017-04-26 02:04:45 +0200

    db

--- models/session.js
+++ models/session.js
@@ -10,13 +10,15 @@ var session = mongoose.model("session", SessionSchema);
 
 
 module.exports = fucntion (user_id) {
-	return session.find({ 'sender_id': user_id }, 'vars', function (err, results) {
-		if (err || !results.length) {
+	//return session.find({ 'sender_id': user_id }, 'vars', function (err, results) {
+	//return session.find({ 'sender_id': user_id }, function (err, results) {
+		return new session({'sender_id' : user_id});
+		/*if (err || !results.length) {
 			console.log("New session created.");
 			return new session({'sender_id' : user_id});
 			//return new session({'sender_id' : user_id, 'vars' : new Object()});
 		}
 		console.log("session found for sender_id = %s", sender_id);
-		return results[0];
-	});
+		return results[0];*/
+	//});
 }
