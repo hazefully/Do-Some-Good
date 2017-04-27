090af8d291736bd111f4141dcd8d0df0a6888ddd
Abdelrahman Ramadan <abdelrahman0xff@gmail.com>
2017-04-27 08:34:26 +0200

    f45t el-donia :D

--- app.js
+++ app.js
@@ -488,12 +488,14 @@ function callSendAPI(messageData) {
       var recipientId = body.recipient_id;
       var messageId = body.message_id;
 
-      console.log("Successfully sent generic message with id %s to recipient %s", 
+      console.log("Successfully sent a message with id %s to recipient %s", 
         messageId, recipientId);
     } else {
       console.error("Unable to send message.");
+      console.log(recipientId);
+      console.log(messageData);
       //console.error(response);
-      console.error(error);
+      //console.error(error);
     }
   });  
 }
